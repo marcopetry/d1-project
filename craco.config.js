7a4b69caca968e8b2eea5d6d3aef041ccf913a8c
marcopetry <marcomattospetry@gmail.com>
2021-04-19 20:36:29 -0300

    feat(core): add packages alias

--- craco.config.js
+++ craco.config.js
@@ -6,6 +6,8 @@ module.exports = {
       '@images': path.resolve(__dirname, 'src/assets/images/'),
       '@fonts': path.resolve(__dirname, 'src/assets/fonts/'),
       '@apis': path.resolve(__dirname, 'src/apis/'),
+      '@hooks': path.resolve(__dirname, 'src/hooks/'),
+      '@providers': path.resolve(__dirname, 'src/providers/'),
     },
   },
 }
