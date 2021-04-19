b8ee994a60ed0ce1687439b2e968f08358823ea2
marcopetry <marcomattospetry@gmail.com>
2021-04-19 18:57:11 -0300

    feat(core): add router-dom ao app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Home } from './pages/Home/Home'
 
 const App = () => {
-  return <Home />
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App
